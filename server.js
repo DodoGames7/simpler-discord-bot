00da5663aba728af80f1c55fa25b6daeed7a71e5
Glitch (glitch-hello-node) <none>
2022-04-14 00:14:27 +0000

    📍🚐 Checkpoint ./server.js:26877849/1045

--- server.js
+++ server.js
@@ -18,6 +18,39 @@ bot.command({
 })
 
 bot.command({
+  name: "testmini",
+  code: `$jsEval[const { TicTacToe } = require('leaf-utils');
+
+await TicTacToe({
+            message: message,
+            slash_command: false,
+            time: 300000,
+            opponent: message.mentions.users.first(),
+            embed: {
+                title: 'Tic Tac Toe',
+                color: 'RED'
+            },
+            challenge: {
+                acceptButton: 'Accept',
+                denyButton: 'Deny',
+            },
+            emojis: {
+                xEmoji: '❌',
+                oEmoji: '🔵',
+            },
+            colors: {
+                xEmoji: 'DANGER',
+                oEmoji: 'PRIMARY',
+            },
+            noUser: 'You must mention someone',
+            acceptMessage: '{{player}} has challenged you {{opponent}}',
+            cancelMessage: '{{opponent}} refused to have a fight with you!',
+            endMessage: 'Challenge not accepted in time',
+            authorOnly: 'You can\'t use these buttons',
+        })]`
+  
+
+bot.command({
   name: "av",
   code: `$send[$channelID[];{createEmbed:{image:$authorAvatar[$mentioned[1]]}}]`
 })
